2ea394dd8c27bdee6ef85f30bebb8d8c9e9b55c9
Savar <savarb@gmail.com>
2018-04-07 18:36:36 +0100

    updated with SPA favorable loading process i.e load index.html (shell of app) if file requested does not exist

--- webserver.go
+++ webserver.go
@@ -3,18 +3,27 @@ package main
 import (
 	"flag"
 	"net/http"
+
+	"os"
+	"strings"
 )
 
 func main() {
-
 	var cPort string
 	flag.StringVar(&cPort, "port", "8080", "Default Port")
-	flag.Parse()
 
-	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(""))))
-
-	println("Strting Server @ port: " + cPort)
+	http.HandleFunc("/", siteHandler)
+	println("Starting Server @ port: " + cPort)
 	if err := http.ListenAndServe(":"+cPort, nil); err != nil {
 		println("Error Starting Webserver: " + err.Error())
 	}
+}
+
+func siteHandler(httpRes http.ResponseWriter, httpReq *http.Request) {
+	urlPath := strings.Split(httpReq.URL.String()[1:], "?")[0]
+	urlPath = strings.Replace(urlPath, "//", "/", -1)
+	if _, err := os.Stat(urlPath); os.IsNotExist(err) {
+		urlPath = "index.html"
+	}
+	http.ServeFile(httpRes, httpReq, urlPath)
 }
